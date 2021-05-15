b259533e68b0bc0e68127c23d6735b6cf052c2f8
Camix94 <camila.oretamales@gmail.com>
2021-05-15 12:55:54 -0400

    Se añade sección de Hazte Socio completa con API

---
+++ js/script-hazte-socio.js
@@ -0,0 +1,16 @@
+
+
+function Mover(obj)
+{
+    obj.innerHTML="Llevo dos meses como socia y me envían una imagen de los animales cada semana!!";
+}
+
+function MoverFuera(obj)
+{
+    obj.innerHTML="María Pérez";
+}
+
+function CambiaColor(a)
+{
+    a.style.background = "orange";
+}
